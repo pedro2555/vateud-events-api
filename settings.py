33a9487784353e993a9dc03a469c7a6ccd336ec3
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-02-14 22:00:07 +0000

    Some more datetime format things, and added missing description fiel

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'user')
 MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'user')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'evedemo')
 
-DATE_FORMAT = '%m-%d-%y\T%H:%M:%S\Z'
+DATE_FORMAT = '%y-%m-%dT%H:%M:%SZ'
 
 X_DOMAINS = ('http://127.0.0.1:8081')
 X_HEADERS = ('content-type', 'if-match')
@@ -54,6 +54,9 @@ events = {
             'type': 'string'
         },
         'subtitle': {
+            'type': 'string'
+        },
+        'description': {
             'type': 'string'
         },
         'airports': {
