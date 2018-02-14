4f5352963c1560afe3e66b6cf65c8a3ef40f4508
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-02-14 21:55:55 +0000

    Ops, some missing syntax

--- settings.py
+++ settings.py
@@ -28,6 +28,8 @@ MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'user')
 MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'user')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'evedemo')
 
+DATE_FORMAT = '%m-%d-%yT%H:%M:%SZ'
+
 X_DOMAINS = ('http://127.0.0.1:8081')
 X_HEADERS = ('content-type', 'if-match')
 
@@ -60,11 +62,11 @@ events = {
         'banner_url': {
             'type': 'string'
         },
-        'start': {
+        'starts': {
             'type': 'datetime'
         },
         'ends': {
-
+            'type': 'datetime'
         }
     }
 }
